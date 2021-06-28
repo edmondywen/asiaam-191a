aabc10dc29b1b3dc33a17476239a41230b0b31ae
edmondywen <edmondywen@gmail.com>
2021-06-27 18:13:07 -0700

    added h1 for title, h2 for section titles, p for paragraghs, added description of self and goals, changed css, used ordered list, included img of self and food

--- week1/js/init.js
+++ week1/js/init.js
@@ -1,19 +1,20 @@
-const map = L.map('map').setView([34.0709, -118.444], 5);
+const map = L.map('db8map').setView([34.0709, -118.444], 10); //argument to map() is id of a div.
 
 // Leaflet tile layer, i.e. the base map
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-//JavaScript let variable declaration to create a marker
-let marker = L.marker([34.0709, -118.444]).addTo(map)
-	.bindPopup('Math Sciences 4328 aka the Technology Sandbox<br> is the lab where I work in ')
-	.openPopup();
+//JavaScript let variable declaration to create a marker. not 100% sure how the code works
+// let marker = L.marker([34.0709, -118.444]).addTo(map)
+// 	.bindPopup('Math Sciences 4328 aka the Technology Sandbox<br> is the lab where I work in ')
+// 	.openPopup();
 
-let marker2 = L.marker([35.0709, -118.444]).addTo(map)
-	.bindPopup('new marker');
+// let marker2 = L.marker([35.0709, -118.444]).addTo(map)
+// 	.bindPopup('new marker');
 
-	fetch("map.geojson")
+//not 100% sure how the code works. no matching promise for the then? i think the fetch provides a promise or something
+fetch("maps/debate.geojson")
 	.then(response => {
 		return response.json();
 		})
